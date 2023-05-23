b04f82d744b8eda158297b6e75158b2966bc9556
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-05-23 12:02:12 +0700

    dashboard siswa sudah selesai

--- application/views/backend/siswa/index.php
+++ application/views/backend/siswa/index.php
@@ -206,19 +206,18 @@
         <div class="col-lg-4 col-md-6">
           <div class="card h-100">
             <div class="card-header pb-0">
-              <h6>Orders overview</h6>
+              <h6>Materi yang Ada pada Media Ini</h6>
               <p class="text-sm">
-                <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
-                <span class="font-weight-bold">24%</span> this month
               </p>
             </div>
             <div class="card-body p-3">
               <div class="timeline timeline-one-side">
                 <div class="timeline-block mb-3">
-                  
+                  <span class="timeline-step">
+                    <i class="material-icons text-danger text-gradient">code</i>
+                  </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">$2400, Design changes</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Flowchart Diagram</h6>
                   </div>
                 </div>
                 <div class="timeline-block mb-3">
@@ -226,44 +225,62 @@
                     <i class="material-icons text-danger text-gradient">code</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">New order #1832412</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 11 PM</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Pseudocode</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"></p>
                   </div>
                 </div>
                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="material-icons text-info text-gradient">shopping_cart</i>
+                    <i class="material-icons text-danger text-gradient">code</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">Server payments for April</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 9:34 PM</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Variable</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"></p>
                   </div>
                 </div>
-                <div class="timeline-block mb-3">
+                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="material-icons text-warning text-gradient">credit_card</i>
+                    <i class="material-icons text-danger text-gradient">code</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">New card added for order #4395133</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">20 DEC 2:20 AM</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Tipe Data</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"></p>
                   </div>
                 </div>
-                <div class="timeline-block mb-3">
+                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="material-icons text-primary text-gradient">key</i>
+                    <i class="material-icons text-danger text-gradient">code</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">Unlock packages for development</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">18 DEC 4:54 AM</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Input Output</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"></p>
                   </div>
                 </div>
-                <div class="timeline-block">
+                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="material-icons text-dark text-gradient">payments</i>
+                    <i class="material-icons text-danger text-gradient">code</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">New order #9583120</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">17 DEC</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Operator</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"></p>
+                  </div>
+                </div>
+                 <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="material-icons text-danger text-gradient">code</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Percabangan</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"></p>
+                  </div>
+                </div>
+                 <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="material-icons text-danger text-gradient">code</i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Perulangan</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"></p>
                   </div>
                 </div>
               </div>
