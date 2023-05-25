70e513f9ae1186684f17f5ad18f1eb329a834800
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-05-25 11:39:34 +0700

    perbaikan header

--- application/views/backend/siswa/sidebar.php
+++ application/views/backend/siswa/sidebar.php
@@ -147,7 +147,7 @@
             </li>
            
             <li class="nav-item d-flex align-items-center">
-              <a href="../pages/sign-in.html" class="nav-link text-body font-weight-bold px-0">
+              <a href="<?php echo base_url('Siswa/profile');?>" class="nav-link text-body font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
                 <span class="d-sm-inline d-none"><?php echo $user['nama'] ;?> </span>
               </a>
