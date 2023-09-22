5afe818e233a88a38b27dec04f8ccdd5d6931385
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-09-22 11:47:57 +0700

    SI PANTEK AKHIRNYA BISA ANJING

--- application/views/admin/globalchat/global.php
+++ application/views/admin/globalchat/global.php
@@ -1,5 +1,22 @@
-  <link rel="stylesheet" href="<?= base_url('assets/css/globalchat.css'); ?>">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('assets/css/globalchat.css'); ?>">
+    <title>Global Chat</title>
+</head>
+<body>
+    <h1>Global Chat</h1>
+    <div class="chat-container">
+        <div class="chat-messages"></div>
+        <div class="chat-input">
+            <textarea id="message" rows="4" placeholder="Your Message"></textarea>
+            <button id="send_message">Send</button>
+        </div>
+    </div>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
             // Refresh chat messages every 2 seconds
@@ -11,7 +28,13 @@
                     success: function(response) {
                         $('.chat-messages').html('');
                         $.each(response, function(index, data) {
-                            var chatMessage = '<div class="chat-message"><strong>' + data.nama + ': </strong>' + data.message + '</div>';
+                            // Mengecualikan tag HTML <stdio.h> dan <iostream>
+                            var message = data.message.replace(/<stdio\.h>|<iostream>/g, function(match) {
+                                return match.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+                            });
+                            // Mengganti karakter baris baru (\n) dengan tag HTML <br>
+                            message = message.replace(/\n/g, '<br>');
+                            var chatMessage = '<div class="chat-message"><strong>' + data.nama + ': </strong>' + message + '</div>';
                             $('.chat-messages').append(chatMessage);
                         });
                     }
@@ -21,6 +44,9 @@
             // Send chat message
             $('#send_message').on('click', function() {
                 var message = $('#message').val();
+
+                // Mengganti karakter baris baru (\n) dengan tag HTML <br> sebelum mengirim ke server
+                message = message.replace(/\n/g, '<br>');
 
                 $.ajax({
                     url: '<?php echo base_url("adminglobalchat/save_message"); ?>',
@@ -35,15 +61,5 @@
             });
         });
     </script>
-
-<body>
-    <h1>Global Chat</h1>
-    <div class="chat-container">
-        <div class="chat-messages"></div>
-        <div class="chat-input">
-            <textarea id="message" rows="4" placeholder="Your Message"></textarea>
-            <button id="send_message">Send</button>
-        </div>
-    </div>
 </body>
-
+</html>
